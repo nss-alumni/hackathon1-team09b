0e643d5146c4cd09f9a37a6be2f1fcb42edbdaca
Adam Oswalt <email@adamoswalt.com>
2017-08-20 16:27:09 -0500

    include full data on search results

--- lib/nss_til_web/controllers/search_controller.ex
+++ lib/nss_til_web/controllers/search_controller.ex
@@ -10,15 +10,42 @@ defmodule NssTilWeb.SearchController do
     """
     with search as (
       select
-        id,
-        text,
+        t.id til_id,
         ts_rank(to_tsvector(t.text), plainto_tsquery('english', $1))
           + similarity(t.text, $1) rank
       from til.til t
+    ),
+    vote as (
+      select
+        til_id,
+        sum(case when value > 0 then 1 else 0 end) upvote_count,
+        sum(case when value < 0 then 1 else 0 end) downvote_count,
+        sum(case when value != 0 then 1 else 0 end) vote_count,
+        sum(value) score
+      from til.vote
+      group by til_id
     )
-    select *
-    from search
-    order by rank desc
+    select
+      t.id,
+      t.text,
+      v.upvote_count,
+      v.downvote_count,
+      v.vote_count,
+      coalesce(v.score, 0) score,
+      t.user_id,
+      t.created_at :: text,
+      u.name user_name,
+      u.image_url user_image,
+      s.rank
+    from til.til t
+    left
+    join vote v
+      on v.til_id = t.id
+    join til.user u
+      on t.user_id = u.id
+    join search s
+      on s.til_id = t.id
+    order by rank desc, score desc, created_at desc
     """
     |> Db.query([query], to_json: true)
     |> Response.send_response(conn)
